6f5f35f855523c03c4ae6b1f8ce3f7202d02d391
eastgyu11 <eastgyu11>
2025-07-15 17:20:54 +0900

    feat: @entity 적용

--- src/main/java/gift/entity/Member.java
+++ src/main/java/gift/entity/Member.java
@@ -1,11 +1,22 @@
 package gift.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(nullable = false, unique = true)
   private String email;
 
+  @Column(nullable = false)
   private String password;
 
   public Member(Long id, String email, String password) {
@@ -43,5 +54,10 @@ public class Member {
 
   public boolean isPasswordNotMatch(String password) {
     return !this.password.equals(password);
+  }
+
+  public void update(String email, String password) {
+    this.email = email;
+    this.password = password;
   }
 }
--- src/main/java/gift/entity/Wish.java
+++ src/main/java/gift/entity/Wish.java
@@ -1,11 +1,29 @@
 package gift.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Wish {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(nullable = false, name = "member_id")
   private Long memberId;
+
+  @Column(nullable = false, name = "product_id")
   private Long productId;
+
   private Long quantity;
+
+  public Wish() {
+
+  }
 
   public Wish(Long id, Long memberId, Long productId, Long count) {
     this.id = id;
@@ -36,5 +54,9 @@ public class Wish {
 
   public void setId(Long id) {
     this.id = id;
+  }
+
+  public void updateQuantity(Long quantity) {
+    this.quantity = quantity;
   }
 }
